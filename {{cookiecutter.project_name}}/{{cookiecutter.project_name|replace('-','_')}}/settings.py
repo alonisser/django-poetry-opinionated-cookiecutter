ff3b864816296c4be7bb97c864dc248e35fc3ec7
omer bental <ombental@gmail.com>
2021-07-20 04:34:50 +0300

    add drf-spectacular autoshcema engine to settings

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name|replace('-','_')}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name|replace('-','_')}}/settings.py
@@ -126,6 +126,7 @@ class Base(Configuration):
         'DEFAULT_AUTHENTICATION_CLASSES': (
             'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         ),
+        'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
         'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
         'DEFAULT_RENDERER_CLASSES': (
