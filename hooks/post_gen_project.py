52e2152fbfa9273c340fc7ccb8c6e99ea99de68f
Alon Wolenitz <67408092+alonw0@users.noreply.github.com>
2024-12-23 11:11:55 +0200

    Add post gen hook for removing heroku files on non-heroku projects

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,13 @@
+import os
+
+# From cookiecutter doc examples: https://cookiecutter.readthedocs.io/en/latest/advanced/hooks.html#example-conditional-files-directories
+
+REMOVE_PATHS = [
+    '{% if cookiecutter.heroku_app_name | length == 0 %}Procfile{% endif %}',
+    '{% if cookiecutter.heroku_app_name | length == 0 %}runtime.txt{% endif %}',
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        os.unlink(path) if os.path.isfile(path) else os.rmdir(path)
