82968181eb19c9c82852a10ef3b6509cff67fbea
Alon Palgy <a.palgy@gmail.com>
2025-01-02 22:10:18 +0200

    added more try except

--- {{cookiecutter.project_name}}/{{cookiecutter.__project_slug}}/locator.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.__project_slug}}/locator.py
@@ -6,8 +6,8 @@ from django.conf import settings
 
 class ServiceNotRegisteredError(Exception):
     def __init__(self, *args, **kwargs):
-        service_name = kwargs.get('service_name')
-        self.message = 'You forgot to register {0}'.format(service_name)
+        service_name = kwargs.get("service_name")
+        self.message = kwargs.get("message", f"Service not registered: {service_name}")
         super().__init__(self.message)
 
 
@@ -17,6 +17,7 @@ class ServiceLocator(object):
     Allowing me to interchange between fake services (for testing) and "real" services for production
     It also allows me to replace certain providers quite easily
     """
+
     singletons = {}
 
     @classmethod
@@ -24,14 +25,24 @@ class ServiceLocator(object):
         settings_name = service_name.upper()
         service_class = getattr(settings, settings_name, None)
         if not service_class:
-            raise ServiceNotRegisteredError(service_name=service_name)
-        module = import_string(service_class)
+            raise ServiceNotRegisteredError(
+                service_name=service_name,
+                message=f"You need to define an attr for {service_name} at the settings file.",
+            )
+        try:
+            module = import_string(service_class)
+        except ImportError:
+            raise ServiceNotRegisteredError(
+                service_name=service_name,
+                message=f"Couldn't import {service_name}. Your import string from settings.py might be wrong: {service_class}",
+            )
+
         return module
 
     @staticmethod
     def is_testing():
-        testing_configurations = ['Testing']
-        return os.environ.get('DJANGO_CONFIGURATION') in testing_configurations
+        testing_configurations = ["Testing"]
+        return os.environ.get("DJANGO_CONFIGURATION") in testing_configurations
 
     @classmethod
     def get_service(cls, service_name):
@@ -40,7 +51,12 @@ class ServiceLocator(object):
 
     @classmethod
     def _get_instance(cls, instance_type):
-        key = instance_type.service_name
+        try:
+            key = instance_type.service_name
+        except AttributeError:
+            raise ServiceNotRegisteredError(
+                message=f"Your service {instance_type} is missing the attr 'service_name'",
+            )
 
         if cls.singletons.get(key) is None:
             cls.singletons[key] = instance_type()
